459f267c74979f5e88244c92556f03719a527b70
lauren <800077081@fvtc.edu>
2026-05-15 02:54:57 -0500

    created app

---
+++ RomanNumeralConverter/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+namespace RomanNumeralConverter;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_OnClicked(object sender, EventArgs e)
+    {
+        var oConvert = new Converter();
+        int InputNumber;
+        var IsNumber = Int32.TryParse(txtInput.Text, out InputNumber);
+
+        if (IsNumber)
+        {
+            lblOutput.Text = oConvert.NumberToRoman(InputNumber);
+        }
+        else
+        {
+            lblOutput.Text = oConvert.RomanToNumber(txtInput.Text).ToString();
+        }
+    }
+}
